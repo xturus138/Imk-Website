5139dcf39eb2f31e816461972845c0da83b07e2b
RyujiKazu <rivalsetiawan200@gmail.com>
2024-02-14 23:21:11 +0700

    menambahkan logika php pada detail kamar

--- Pemilik/DetailListKamar.php
+++ Pemilik/DetailListKamar.php
@@ -13,17 +13,32 @@
           }
     </style>
     <?php
-        require("../koneksi.php");
-        $listkamar = $_POST['listkamar'];
-        $sql = "SELECT * FROM penghuni WHERE listkamar = '$listkamar'";
-        $hasil = mysqli_query($conn, $sql);
-    ?>
+    require("../koneksi.php");
+    $listkamar = $_POST['listkamar'];
+    $sql = "SELECT * FROM penghuni WHERE listkamar = '$listkamar'";
+    $hasil = mysqli_query($conn, $sql);
+
+    $dataPenghuni = "";
+    while ($row = mysqli_fetch_assoc($hasil)) {
+        $dataPenghuni .= $row['nama'] . " ";
+    }
+?>
+
+<body style="background-color:paleturquoise;">
+    <div class="w3-animate-zoom">
+    <style>
+        .ccontainer {
+            display: flex;
+            align-items: top;
+            justify-content: center;
+          }
+    </style>
         <div class="w3-col w3-container w3-margin-top ccontainer" style="width:100%; position: relative;">
-            <label style="position: absolute; pointer-events: none;"><h3>Data Penghuni Kamar -</h3></label>
-            <textarea class="w3-input w3-border w3-round-xxlarge" id="DataPenghuni" readonly style="padding-top: 75px; height: 550px;"></textarea>
-        </div>
-        <button class="w3-button w3-green w3-margin-top w3-round-xxlarge" style="width:10%; position: fixed; bottom: 50px; right: 165px;">EDIT</button>
-        <button class="w3-button w3-green w3-margin-top w3-round-xxlarge" style="width:10%; position: fixed; bottom: 50px; right: 15px;">HAPUS</button>        
-    </div>
-</body>
-</html> 
+            <label style="position: absolute; pointer-events: none;"><h3>Data Penghuni Kamar - <?php echo $dataPenghuni; ?> </h3></label>
+            <textarea class="w3-input w3-border w3-round-xxlarge" id="DataPenghuni" readonly style="padding-top: 75px; height: 550px;"></textarea> 
+        </div> 
+        <button class="w3-button w3-green w3-margin-top w3-round-xxlarge" style="width:10%; position: fixed; bottom: 50px; right: 165px;">EDIT</button> 
+        <button class="w3-button w3-green w3-margin-top w3-round-xxlarge" style="width:10%; position: fixed; bottom: 50px; right: 15px;">HAPUS</button> 
+    </div> 
+</body> 
+</html>
--- Pemilik/listKamarPemilik.php
+++ Pemilik/listKamarPemilik.php
@@ -63,39 +63,39 @@
         </div>
         <div class="w3-row">
             <div class="w3-col marginCustom">
-                <button class="w3-button occupied w3-ripple w3-hover-green" onclick="link()"><h1>Kamar 2</h1><span class="status-text">Diisi</span></button>
+                <button type=submit name="listkamar" class="w3-button occupied w3-ripple w3-hover-green" value="kamar2" onclick="link()"><h1>Kamar 2</h1><span class="status-text">Diisi</span></button>
             </div>
         </div>
         <div class="w3-row">
             <div class="w3-col marginCustom">
-                <button class="w3-button w3-ripple w3-hover-green" onclick="link()"><h1>Kamar 3</h1><span class="status-text">Kosong</span></button>
+                <button type=submit name="listkamar" class="w3-button w3-ripple w3-hover-green" value="kamar3" onclick="link()"><h1>Kamar 3</h1><span class="status-text">Kosong</span></button>
             </div>
         </div>
         <div class="w3-row">
             <div class="w3-col marginCustom">
-                <button class="w3-button w3-ripple w3-hover-green" onclick="link()"><h1>Kamar 4</h1><span class="status-text">Diisi</span></button>
+                <button type=submit name="listkamar" class="w3-button w3-ripple w3-hover-green" value="kamar4" onclick="link()"><h1>Kamar 4</h1><span class="status-text">Diisi</span></button>
             </div>
         </div>
     </div>
     <div class="w3-half w3-container">
         <div class="w3-row">
             <div class="w3-col marginCustom">
-                <button class="w3-button w3-ripple w3-hover-green" onclick="link()"><h1>Kamar 5</h1><span class="status-text">Kosong</span></button>
+                <button type=submit name="listkamar" class="w3-button w3-ripple w3-hover-green" value="kamar5" onclick="link()"><h1>Kamar 5</h1><span class="status-text">Kosong</span></button>
             </div>
         </div>
         <div class="w3-row">
             <div class="w3-col marginCustom">
-                <button class="w3-button w3-ripple w3-hover-green" onclick="link()"><h1>Kamar 6</h1><span class="status-text">Diisi</span></button>
+                <button type=submit name="listkamar" class="w3-button w3-ripple w3-hover-green" value="kamar6" onclick="link()"><h1>Kamar 6</h1><span class="status-text">Diisi</span></button>
             </div>
         </div>
         <div class="w3-row">
             <div class="w3-col marginCustom">
-                <button class="w3-button w3-ripple w3-hover-green occupied" onclick="link()"><h1>Kamar 7</h1><span class="status-text">Kosong</span></button>
+                <button type=submit name="listkamar" class="w3-button w3-ripple w3-hover-green occupied" value="kamar7" onclick="link()"><h1>Kamar 7</h1><span class="status-text">Kosong</span></button>
             </div>
         </div>
         <div class="w3-row">
             <div class="w3-col marginCustom">
-                <button class="w3-button w3-ripple w3-hover-green occupied" onclick="link()"><h1>Kamar 8</h1><span class="status-text">Diisi</span></button>
+                <button type=submit name="listkamar" class="w3-button w3-ripple w3-hover-green occupied" value="kamar8" onclick="link()"><h1>Kamar 8</h1><span class="status-text">Diisi</span></button>
             </div>
         </div>
     </div>

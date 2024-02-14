5d3d0562180b351bad476dab005013340e530bd2
xturus138 <radityaramadhan138@gmail.com>
2024-02-15 02:07:59 +0700

    harus pasang pengumuman.sql dan beres pengumuman pemilik

--- Pemilik/PengumumanPemilik.php
+++ Pemilik/PengumumanPemilik.php
@@ -1,37 +1,55 @@
-    <html>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../w3.css">
-        <head>
-            <title>Pengumuman</title>
-        </head>
-        <body style="background-color:paleturquoise;">
-            <div>
-                <style>
-                    .w3-input::placeholder {
-                        text-align: left;
-                        padding: 10px;
-                        font-size: 14px; 
-                        position: relative;
-                        color: black;
-                        top: 0;
-                        left: 0;
-                        width: 100%;
-                    }
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../w3.css">
+    <title>Pengumuman</title>
+    <style>
+        .w3-input::placeholder {
+            text-align: left;
+            padding: 10px;
+            font-size: 14px; 
+            position: relative;
+            color: black;
+            top: 0;
+            left: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+<body style="background-color:paleturquoise;">
+    <?php
+        require("../koneksi.php");
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['inputPengumuman'])) {
+            $updatedPengumuman = $_POST['inputPengumuman'];
+            $sqlUpdate = "UPDATE pengumuman SET isipengumuman = '$updatedPengumuman'"; 
+            if(mysqli_query($conn, $sqlUpdate)) {
+                echo '<script>alert("Pengumuman berhasil ditambahkan.");</script>';
+            } else {
+                echo '<script>alert("Pengumuman tidak berhasil ditambahkan.");</script>';
+            }
+        }
+        $sqlPengumuman = "SELECT isipengumuman FROM pengumuman";
+        $resultPengumuman = mysqli_query($conn, $sqlPengumuman);
+        $pengumumanList = "";
+        while ($row = mysqli_fetch_assoc($resultPengumuman)) {
+            $pengumumanList .= $row['isipengumuman'] . "\n";
+        }
+    ?>
 
-                </style>
+    <div class="w3-container w3-margin w3-animate-zoom">
+        <form method="post" action="">
+            <div style="position: relative;">
+                <label for="inPengumuman" style="position: absolute; top: 5px; left: 10px;">Tulis Pengumuman Disini</label>
+                <textarea id="inPengumuman" class="w3-input w3-border w3-round-large" name="inputPengumuman" style="width: 100%; height: 150px; overflow:hidden; padding-top: 30px;"></textarea>
             </div>
-            <div class="w3-container w3-margin w3-animate-zoom">
-                <div style="position: relative;">
-                    <label for="inlap" style="position: absolute; top: 5px; left: 10px;">Tulis Pengumuman Disini</label>
-                    <textarea id="inlap" class="w3-input w3-border w3-round-large" name="inputLaporan" style="width: 100%; height: 150px; overflow:hidden; padding-top: 30px;"></textarea>
-                </div>
-                <button class="w3-button w3-green w3-margin-top w3-round-xxlarge" style="width:15%">Kirim</button>
-                <div style="position: relative;">
-                    <label for="outlap" style="position: absolute; top: 5px; left: 10px;">List Pengumuman</label>
-                    <textarea id="outlap" class="w3-input w3-border w3-round-large w3-margin-top" name="outputLaporan" readonly type="text" style="width: 100%; height: 150px; overflow:hidden; padding-top: 30px;"></textarea>
-                </div>
-               
-               
-            </div>
-        </body>
-    </html>
+            <button type="submit" class="w3-button w3-green w3-margin-top w3-round-xxlarge" style="width:15%">Kirim</button>
+        </form>
+        <div style="position: relative;">
+            <label for="outPengumuman" style="position: absolute; top: 5px; left: 10px;">List Pengumuman</label>
+            <textarea id="outPengumuman" class="w3-input w3-border w3-round-large w3-margin-top" name="outputPengumuman" readonly type="text" style="width: 100%; height: 150px; overflow:hidden; padding-top: 30px;"><?php echo $pengumumanList; ?></textarea>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ pengumuman.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 14, 2024 at 08:05 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kosan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengumuman`
+--
+
+CREATE TABLE `pengumuman` (
+  `listpengumuman` varchar(10) NOT NULL,
+  `isipengumuman` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengumuman`
+--
+
+INSERT INTO `pengumuman` (`listpengumuman`, `isipengumuman`) VALUES
+('pengumuman', 'tterrereereer');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  ADD PRIMARY KEY (`listpengumuman`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

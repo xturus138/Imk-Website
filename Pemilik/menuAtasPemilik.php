0afda5d8cd06fb40b3a2653042a8f725c0c6c1d3
xturus138 <radityaramadhan138@gmail.com>
2024-02-15 22:02:14 +0700

    menuatas

--- Pemilik/menuAtasPemilik.php
+++ Pemilik/menuAtasPemilik.php
@@ -3,6 +3,11 @@
     <title>Aplikasi Kosan</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../w3.css">
+    <style>
+        .navbar-button {
+                width: 10% !important;
+            }
+    </style>
     <script>
         function Logout() {
             window.top.location.href = "alert.php"; 
@@ -11,16 +16,16 @@
 <body>
     <div class="w3-bar w3-blue">
         <a href="PemilikLobby.php" target="bawah">
-            <button class="w3-bar-item w3-button  w3-border-right">Lobby</button></a>
+            <button class="w3-bar-item w3-button  w3-border-right navbar-button">Lobby</button></a>
         <a href="PengumumanPemilik.php" target="bawah">
-            <button class="w3-bar-item w3-button  w3-border-right">Pengumuman</button></a>
+            <button class="w3-bar-item w3-button  w3-border-right navbar-button">Pengumuman</button></a>
         <a href="LaporanPemilik.php" target="bawah">
-            <button class="w3-bar-item w3-button  w3-border-right">Laporan</button></a>
+            <button class="w3-bar-item w3-button  w3-border-right navbar-button">Laporan</button></a>
         <a href="PembayaranPemilik.php" target="bawah">
-            <button class="w3-bar-item w3-button  w3-border-right">Pembayaran</button></a>
+            <button class="w3-bar-item w3-button  w3-border-right navbar-button">Pembayaran</button></a>
         <a href="ListKamarPemilik.php" target="bawah">
-            <button class="w3-bar-item w3-button  w3-border-right">List Kamar</button></a>
-        <button class="w3-bar-item w3-button w3-right w3-red  w3-border-right" onclick="Logout()">Logout</button>
+            <button class="w3-bar-item w3-button  w3-border-right navbar-button">List Kamar</button></a>
+        <button class="w3-bar-item w3-button w3-right w3-red w3-border-right navbar-button" onclick="Logout()">Logout</button>
     </div>
 </body>
 </html>
--- Penghuni/menuAtasPenghuni.php
+++ Penghuni/menuAtasPenghuni.php
@@ -3,6 +3,11 @@
     <title>Kost</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../w3.css">
+    <style>
+        .navbar-button {
+                width: 10% !important;
+            }
+    </style>
     <script>
         function Logout() {
             window.top.location.href = "alert.php"; 
@@ -12,15 +17,15 @@
 <body>
     <div class="w3-bar w3-blue">
         <a href="PenghuniLobby.php" target="bawah">
-            <button class="w3-bar-item w3-button  w3-border-right">Lobby</button>
+            <button class="w3-bar-item w3-button  w3-border-right navbar-button">Lobby</button>
         </a>
         <a href="laporanPenghuni.php" target="bawah">
-            <button class="w3-bar-item w3-button  w3-border-right">Laporan</button>
+            <button class="w3-bar-item w3-button  w3-border-right navbar-button">Laporan</button>
         </a>
         <a href="listKamarPenguni.php" target="bawah">
-            <button class="w3-bar-item w3-button  w3-border-right">List Kamar</button>
+            <button class="w3-bar-item w3-button  w3-border-right navbar-button">List Kamar</button>
         </a>
-        <button class="w3-bar-item w3-button w3-right w3-red  w3-border-right" onclick="Logout()">Logout</button>
+        <button class="w3-bar-item w3-button w3-right w3-red  w3-border-right navbar-button" onclick="Logout()">Logout</button>
     </div>
 </body>
 </html>
